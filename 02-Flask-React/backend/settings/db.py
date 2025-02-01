124d1f31c7b554ad652dcbd43e175e17b14a4ae6
arnoldbgm <arnold.gallegosm@gmail.com>
2025-02-01 17:48:48 -0500

    ⚡Feat: Change Flask

--- 02-Flask-React/backend/settings/db.py
+++
@@ -1,7 +0,0 @@
-# Aqui vamos a inicializar la conexion a nuestro servidor de base de datos
-# Aqui nos CONECTAMOS A LA BD 💣
-from flask_sqlalchemy import SQLAlchemy
-
-# Porque lo creo separado este settings, es porque lo vamos a usar en varios archivos
-
-db = SQLAlchemy()
